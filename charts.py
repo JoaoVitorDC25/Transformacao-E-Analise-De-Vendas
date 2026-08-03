e75ef743cf2d2d728badd605e61e2317f839c264
João Vitor Dias <joaovitordiasc.25@gmail.com>
2026-08-03 00:24:20 -0300

    feat: add grafico_bar function in charts.py

--- charts.py
+++ charts.py
@@ -1,8 +1,7 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-from config import FIGURE_SIZE
-from config import ESTILO_GRAFICO
+from config import FIGURE_SIZE, TITLE_SIZE, ESTILO_GRAFICO, LABEL_SIZE
 
 plt.rcParams['figure.figsize'] = FIGURE_SIZE
 
@@ -21,3 +20,30 @@ def grafico_bloxpot(x, title):
     sns.boxplot(x = x)
     plt.title(title)
     plt.show()
+    
+def grafico_bar(
+    dados,
+    titulo,
+    yLabel,
+    xLabel,
+    xTicks = 0
+    ):
+
+    dados.plot(kind = 'bar', color = 'skyblue')
+    
+    plt.title(titulo, fontsize = TITLE_SIZE)
+    plt.ylabel(yLabel, fontsize = LABEL_SIZE)
+    plt.xlabel(xLabel, fontsize = LABEL_SIZE)
+    plt.xticks(rotation = xTicks)
+    plt.show()
+#plt.ylabel('Receita (R$)')
+#plt.xlabel('Categoria')
+#plt.xticks(rotation = 0)
+
+   
+#def grafico_barh():
+    
+#def grafico_line():
+    
+#def grafico_pie():
+    
