06886b39c34d8852866d89ab93d736ef9d7d15f6
João Vitor Dias <joaovitordiasc.25@gmail.com>
2026-08-02 21:30:30 -0300

    feat: create grafico_bloxpot function

--- charts.py
+++ charts.py
@@ -1,7 +1,23 @@
 import seaborn as sns
+import matplotlib.pyplot as plt
+
+from config import FIGURE_SIZE
+from config import ESTILO_GRAFICO
+
+plt.rcParams['figure.figsize'] = FIGURE_SIZE
 
 def configurar_estilo():
     """
     Configurar o estilo do padrão de gráfico
     """   
-    sns.set_style("darkgrid")
+    sns.set_style(ESTILO_GRAFICO)
+    
+def grafico_bloxpot(x, title):
+    """
+    Função para criar um gráfico de boxplot
+    """
+    configurar_estilo()
+    
+    sns.boxplot(x = x)
+    plt.title(title)
+    plt.show()
